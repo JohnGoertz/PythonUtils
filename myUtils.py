8862bb2dcf2ad9753032f01a6ef457633159355a
johngoertz <johngoertz@gmail.com>
2019-08-19 15:56:25 +0100

    Classical and bootstrap confidence intervals for curve fits

--- myUtils.py
+++ myUtils.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import datetime #For timestamping files
 import pathlib as pl
 import shelve #For saving/loading variables
+import scipy.stats as stats
+import scipy.optimize as opt
+import numpy as np
+from tqdm import tqdm 
 
 
 def plotSettings():
@@ -76,3 +80,108 @@ def savemyfig(fig_obj, title, path = pl.Path.cwd()):
     fig_obj.savefig(path / (title+'.svg'), bbox_inches='tight')
     print('Done')
     return
+
+
+#Fit specified curve and return 95% confidence and prediction intervals
+def curve_fit_ci(func, x, y, method = 'bootstrap', n_steps = 500, res = 100, xpts = None, fit_kwargs = {}):
+    
+    if y.ndim == 1:
+        p_opt, p_cov = opt.curve_fit(func, x, y, **fit_kwargs)
+    else:
+        p_opt, p_cov = opt.curve_fit(func, x, y.mean(1), sigma = y.std(1), absolute_sigma = True, **fit_kwargs)
+        
+    gof = {'classical': classical_ci(func, x, y, p_opt, p_cov, res, xpts),
+           'bootstrap': None}
+    return gof.get(method,None)
+
+
+def classical_ci(func, x, y, p_opt, p_cov, res = 100, xpts = None):
+    if x.size != y.size:
+        x = np.tile(x,[y.size//x.size,1]).T
+
+    n = y.size
+    m = p_opt.size
+    dof = n-m                                                # Degrees of freedom
+    resid = y - func(x,*p_opt)                                 # Residuals
+    t = stats.t.ppf(0.975, n - m)                            # Student's t distribution
+    chi2 = np.sum((resid / func(x,*p_opt))**2)                 # chi-squared; estimates error in data
+    chi2_red = chi2 / dof                                    # reduced chi-squared; measures goodness of fit
+    s_err = np.sqrt(np.sum(resid**2) / dof)                    # standard error of the fit at each point
+
+    ss_resid = np.sum(resid**2)
+    ss_tot = np.sum((y-np.mean(y))**2)
+    r2 = 1 - (ss_resid / ss_tot)
+
+    if xpts is None: xpts = np.linspace(x.min,x.max,res)
+    elif xpts.size == 2: xpts = np.linspace(xpts[0],xpts[1],res)
+        
+    ci = t * s_err * np.sqrt(1/n + (xpts - np.mean(x))**2 / np.sum((x - np.mean(x))**2))
+
+    pi = t * s_err * np.sqrt(1 + 1/n + (xpts - np.mean(x))**2 / np.sum((x - np.mean(x))**2))
+
+    return {'95% CI' : ci, '95% PI' : pi, 'Reduced Chi^2' : chi2_red, 'R^2' : r2}
+
+
+
+def bootstrap_ci(func, x, y, p_opt, n_straps = 500, res = 100, xpts = None,
+                 alpha = 0.05, fit_kwargs = {}, conservative = True, piecewise = True):
+    
+    if y.ndim == 1: piecewise = False
+    
+    if xpts is None: xpts = np.linspace(x.min(),x.max(),res)
+    elif xpts.ndim == 2: xpts = np.linspace(xpts[0],xpts[1],res)
+
+    y_fit = func(x,*p_opt)
+    
+    if y.shape != x.shape:
+        y_fit = np.tile(y_fit,[y.size//x.size,1]).T
+        
+    resid = y - y_fit                                 # Residuals
+    sigma_resid = resid.std()
+    total_sigma = sigma_resid #np.sqrt(sigma_resid**2 + syst_err**2)        
+    
+    p_strapped = np.tile(np.zeros(p_opt.size),(n_straps,1))
+    
+    curve_strapped = np.tile(np.zeros(xpts.size),(n_straps,1))
+    
+    for i in tqdm(range(n_straps)):
+        
+        if piecewise and conservative:
+             resid_resamples = np.array([np.random.choice(resid[row],size = resid[row].shape) for row in range(len(x))])
+        elif piecewise and not conservative:
+            resid_resamples = np.array([np.random.normal(0,size = resid[row].shape) for row in range(len(x))])
+        elif not piecewise and not conservative:
+            resid_resamples = np.random.normal(0, total_sigma, size = resid.shape)
+        elif not piecewise and conservative:
+            resid_resamples = np.random.choice(resid.flat, size = resid.shape)
+              
+        new_y = y+resid_resamples
+        
+        if y.ndim == 1:
+            p_strapped[i], _ = opt.curve_fit(func, x, new_y, **fit_kwargs)
+        else:
+            p_strapped[i], _ = opt.curve_fit(func, x, new_y.mean(1), sigma = y.std(1), absolute_sigma = True, **fit_kwargs)
+        
+        curve_strapped[i] = func(xpts,*p_strapped[i])
+    
+    lower_bound = (alpha/2)*100
+    upper_bound = (1-alpha/2)*100
+    
+    return {'Param mean'  : p_strapped.mean(0),
+            'Param lower' : np.percentile(p_strapped,lower_bound,axis = 0),
+            'Param median': np.percentile(p_strapped,50,axis = 0),
+            'Param upper' : np.percentile(p_strapped,upper_bound,axis = 0),
+            'Curve mean'  : curve_strapped.mean(0),
+            'Curve lower' : np.percentile(curve_strapped,lower_bound,axis = 0),
+            'Curve median': np.percentile(curve_strapped,50,axis = 0),
+            'Curve upper' : np.percentile(curve_strapped,upper_bound,axis = 0)}
+            
+
+    
+    
+    
+    
+
+
+
+
