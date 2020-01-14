126ca98a2162625cd445bced50f8f48ea852073a
johngoertz <johngoertz@gmail.com>
2020-01-14 11:59:43 +0000

    CompetitiveReaction class uses jitcode compilation

--- PCRmodels.py
+++ PCRmodels.py
@@ -7,13 +7,62 @@ import ipywidgets as ipw
 
 from tqdm.notebook import tqdm
 
-import symfit as sf
+import jitcode as jc
+#https://aip-scitation-org.iclibezp1.cc.ic.ac.uk/doi/10.1063/1.5019320
+import symengine as se
 
 import shelve
 import gc
 import warnings
 
+
+class Argument():
+    def __init__(self, name):
+        self.name = name
+        self.sym = se.Symbol(name)
+        
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.__str__()
+
+class Parameter(Argument):    
+    def __init__(self, name, value=1.0):
+        self.value = value
+        super(Parameter, self).__init__(name)
+        
+class Variable(Argument):
+    def __init__(self, name, var_list, init=1.0):
+        self.y = jc.y(len(var_list))
+        self.init = init
+        super(Variable, self).__init__(name)
+        var_list.append(self)
+        
+def copies2molar(copies):
+    #Calculates concentration in a 10 μL volume
+    return copies / 6.022e23 / (10*10**-6)
+
+def molar2copies(moles):
+    #Calculates number of copies in a 10 μL volume
+    return moles * 6.022e23 * (10*10**-6)
+
 class CompetitiveReaction:
+    
+    defaults = {
+        'norm' : molar2copies(120*10**-9),
+        'cycles' : 60,
+        'INT_res' : 1.,
+        'INT_rng' : [1,9],
+        'diffs' : None,
+        'solution' : None,
+        'frozen' : False,
+        'sweep_INT_idx' : 0,
+        'oligo_init' : 10**5,
+        'rate' : 1,
+        'primer_init' : 120
+    }
+    
     def __init__(self, INT_inputs, EXT_inputs, labeled_strands, warnings=True):
         self.INT_inputs = INT_inputs
         self.INTs = list(INT_inputs.keys())
@@ -27,12 +76,12 @@ class CompetitiveReaction:
         assert all([len(pair)==2 for pair in self.input_oligos.values()]), 'All strands must have exactly two primers'
         self._primers_list = list(sorted(set(primer for pair in self.input_oligos.values() for primer in pair)))
         self.oligos = list(self.input_oligos.keys())
-        self.checkUnique('oligos')
         self.buildConnections()
         self.buildComponents()
         self.buildEquations()
-        self.set_defaults()
+        self.setDefaults()
         self.buildLabels()
+        self.compileODE()
             
     ################################################################################
     ## Convenience Functions
@@ -43,16 +92,6 @@ class CompetitiveReaction:
     
     def from_list(self,attribute,item):
         return getattr(self,attribute)[self.list(attribute).index(item)]
-    
-    @staticmethod
-    def copies2molar(copies):
-        #Calculates concentration in a 10 μL volume
-        return copies / 6.022e23 / (10*10**-6)
-
-    @staticmethod
-    def molar2copies(moles):
-        #Calculates number of copies in a 10 μL volume
-        return moles * 6.022e23 * (10*10**-6)
         
     ################################################################################
     ## Setters and Getters
@@ -65,23 +104,43 @@ class CompetitiveReaction:
         assert rate_name in self.list('rates'), f'Oligo {oligo} not found'
         self.from_list('rates',rate_name).value = rate
         
+    def get_rate(self, oligo):
+        oligo = str(oligo)
+        rate_name = 'r_'+oligo
+        assert rate>0, 'Rate must be greater than 0'
+        assert rate_name in self.list('rates'), f'Oligo {oligo} not found'
+        return self.from_list('rates',rate_name).value
+        
     def set_primer_init(self, primer, nM):
         primer = str(primer)
-        assert primer+'_0' in self.list('primer_inits'), f'Primer {primer} not found'
-        self.from_list('primer_inits',primer+'_0').value = nM
+        assert primer in self.list('primers'), f'Primer {primer} not found'
+        # Set the init in copies
+        self.from_list('primers',primer).init = molar2copies(nM*10**-9)
+        
+    def get_primer_init(self, primer, copies=False):
+        primer = str(primer)
+        assert primer in self.list('primers'), f'Primer {primer} not found'
+        if copies:
+            # Init should already be in copies
+            return self.from_list('primers',primer).init
+        else:
+            return copies2molar(self.from_list('primers',primer).init)*10**9
         
     def set_oligo_init(self, oligo, copies):
         oligo = str(oligo)
         assert oligo in self.list('oligos'), f'Oligo {oligo} not found'
-        self.from_list('strand_inits',oligo+'_L_0').value = copies
-        self.from_list('strand_inits',oligo+'_R_0').value = copies
+        self.from_list('strands',oligo+'_L').init = copies
+        self.from_list('strands',oligo+'_R').init = copies
                 
     def get_oligo_init(self, oligo):
         oligo = str(oligo)
         assert oligo in self.list('oligos'), f'Oligo {oligo} not found'
-        strand_inits = [strand.value for strand in self.strand_inits if str(strand)[:-4]==oligo]
-        assert all(init==strand_inits[0] for init in strand_inits)
-        return strand_inits[0]
+        inits = [strand.init for strand in self.strands if str(strand)[:-2]==oligo]
+        # Ensure there are exactly two strands for the oligo
+        assert len(inits)==2
+        # Ensure both inits are the same
+        assert all(init==inits[0] for init in inits)
+        return inits[0]
     
     ################################################################################
     ## Configure the necessary elements for the simulations
@@ -108,108 +167,190 @@ class CompetitiveReaction:
         return connections
     
     def buildComponents(self):
+        var_list=[]
+        
         INT_strands_list = ['_'.join(strand) for strand in self.connections.index.to_list() if strand[0] in self.list('INTs')]
         EXT_strands_list = ['_'.join(strand) for strand in self.connections.index.to_list() if strand[0] in self.list('EXTs')]
-        
-        self.INT_strands = sf.variables(','.join(INT_strands_list))
-        self.EXT_strands = sf.variables(','.join(EXT_strands_list))
+
+        self.INT_strands = [Variable(strand, var_list) for strand in INT_strands_list]
+        self.EXT_strands = [Variable(strand, var_list) for strand in EXT_strands_list]
         self.strands = self.INT_strands+self.EXT_strands
-        
-        self.primers = sf.variables(','.join(self._primers_list))
-        self.checkUnique('primers')
-        self.rates = sf.parameters(','.join('r_'+oligo for oligo in self.list('oligos')))
-        
-        self.c = sf.Variable('c')
-        
-        self.INT_inits = sf.parameters(','.join([INT+'_0' for INT in INT_strands_list]))
-        self.EXT_inits = sf.parameters(','.join([EXT+'_0' for EXT in EXT_strands_list]))
+
+        self.primers = [Variable(primer, var_list) for primer in self._primers_list]
+        rate_list = ['r_'+oligo for oligo in self.list('oligos')]
+        self.rates = [Parameter(rate) for rate in rate_list]
+
+        self.INT_inits = [Parameter(INT+'_0') for INT in INT_strands_list]
+        self.EXT_inits = [Parameter(EXT+'_0') for EXT in EXT_strands_list]
         self.strand_inits = self.INT_inits+self.EXT_inits
-        self.primer_inits = sf.parameters(','.join([str(primer)+'_0' for primer in self.primers]))
-        
-    def checkUnique(self,attribute):
-        if not self.warn: return
-        for obj in gc.get_objects():
-            if obj is self: continue
-            if isinstance(obj, self.__class__):
-                for item in getattr(self,attribute):
-                    if item in getattr(obj,attribute):
-                        warnings.warn(f'None-unique name {item} in {attribute}. '+
-                                      'These will be created as the same object, which may cause unexpected behavior')
-    
+        self.primer_inits = [Parameter(str(primer)+'_0') for primer in self.primers]
+
     def buildEquations(self):
 
         lg2_e = np.log2(np.exp(1))
         def get_strand(strand): return self.from_list('strands',strand)
-
+        
+        # Lookup table for which strands use a given primer
         strands_per_primer = {
-            primer: ['_'.join(strand) for strand in self.connections.index[self.connections[str(primer)]]]
+            primer: ['_'.join(idx) for idx in self.connections.index[self.connections[str(primer)]]]
             for primer in self.primers
         }
 
+        # Lookup table for which primers are used by a given primer
         primer_per_strand = {
-            get_strand('_'.join(idx)): self.primers[i] for idx,row in self.connections.iterrows() for i,v in enumerate(row[self.list('primers')]) if v
+            get_strand('_'.join(idx)): self.primers[i] for idx,row in self.connections.iterrows() 
+            for i,v in enumerate(row[self.list('primers')]) if v
         }
 
+        # Rate-limiting coefficients
+        # The 'half-rate' concentration is the 
         mu = {
-            primer : primer/(sum(self.strands[i] for i,strand in enumerate(self.list('strands')) if strand in strands_per_primer[primer]) + primer) 
+            primer : primer.y/(sum(self.strands[i].y for i,strand in enumerate(self.list('strands')) if strand in strands_per_primer[primer]) + primer.y) 
             for primer in self.primers
         }
-        
-        c = self.c
-        
-        eqns = {oligo : {
-            sf.D(get_strand(oligo+'_R'),c): self.rates[i]/lg2_e*get_strand(oligo+'_L')*mu[primer_per_strand[get_strand(oligo+'_R')]],
-            sf.D(get_strand(oligo+'_L'),c): self.rates[i]/lg2_e*get_strand(oligo+'_R')*mu[primer_per_strand[get_strand(oligo+'_L')]]
-        } for i,oligo in enumerate(self.list('oligos'))}
-        
+
+        eqns = {
+            oligo : {
+                get_strand(oligo+'_R').y: self.rates[i].sym/lg2_e*get_strand(oligo+'_L').y*mu[primer_per_strand[get_strand(oligo+'_R')]],
+                get_strand(oligo+'_L').y: self.rates[i].sym/lg2_e*get_strand(oligo+'_R').y*mu[primer_per_strand[get_strand(oligo+'_L')]]
+            } for i,oligo in enumerate(self.list('oligos'))
+        }
+
         # Flatten the nested dictionaries to a single level
         eqns = {deriv: expr for oligo in self.list('oligos') for deriv,expr in eqns[oligo].items()}
 
         eqns.update({
-            sf.D(primer,c): -sum(eqns[sf.D(get_strand(strand),c)] for strand in strands_per_primer[primer])
+            primer.y: -sum(eqns[get_strand(strand).y] for strand in strands_per_primer[primer])
         for primer in self.primers})
-        
+
         self.eqns = eqns
-        
+
         return eqns
+    
+    def setDefaults(self):
+        dflt = self.defaults
+        self.norm = dflt['norm']
+        self.sweep_INT = self.INTs[dflt['sweep_INT_idx']]
+        self.INT_rng = dflt['INT_rng']
+        self.INT_res = dflt['INT_res']
+        self.cycles = dflt['cycles']
+        self.diffs = dflt['diffs']
+        self.solution = dflt['solution']
+        self.frozen = dflt['frozen']
+        for oligo in self.oligos:
+            self.set_oligo_init(oligo,dflt['oligo_init'])
+            self.set_rate(oligo,dflt['rate'])
+        for primer in self.primers:
+            self.set_primer_init(primer,dflt['primer_init'])
     
     def buildLabels(self):
         label1_strands_list = ['_'.join(oligo) for oligo,row in self.connections.iterrows() if row.Label == self.list('labels')[0]]
         self.label1_strands = [self.from_list('strands',strand) for strand in label1_strands_list]
         label2_strands_list = ['_'.join(oligo) for oligo,row in self.connections.iterrows() if row.Label == self.list('labels')[1]]
         self.label2_strands = [self.from_list('strands',strand) for strand in label2_strands_list]
-    
-    def set_defaults(self):
-        for oligo in self.oligos:
-            self.set_oligo_init(oligo,10**5)
-            self.set_rate(oligo,1)
-        for primer in self.primers:
-            self.set_primer_init(primer,120)
-        self.norm = self.molar2copies(120*10**-9)
-        self.sweep_INT = self.INTs[0]
-        self.INT_rng = [1,9]
-        self.INT_res = 1
-        self.cycles = 60
-        self.diffs = None
-        self.solution = None
         
     ################################################################################
     ## Running a simulation
     ################################################################################
     
-    def initialize(self):
-        #self.connections = self.buildConnections()
-        #TODO: Update equations without clearing values
-        initial = {self.c:0.0}
-        initial.update({p:self.molar2copies(p0.value*1e-9) for p,p0 in zip(self.primers,self.primer_inits)})
-        initial.update({strand: init.value for strand,init in zip(self.strands,self.strand_inits)})
-        return initial
     
-    def solve(self,initial):
-        self.model = sf.ODEModel(self.eqns, initial=initial)
-        solution = self.model(c=np.arange(self.cycles), **{str(rate):rate.value for rate in self.rates})._asdict()
-        self.solution = {str(k):v for k,v in solution.items()}
-        return self.solution
+    
+    def freezeEquations(self):
+        '''Replace symbolic parameters in equations with explicit values'''
+        self.eqns = {k:v.subs({rate.sym:rate.value for rate in self.rates}) for k,v in self.eqns.items()}
+        self.frozen = True
+        
+    def unfreezeEquations(self):
+        '''Reset equations to use symbolic parameters'''
+        self.buildEquations()
+        self.frozen = False
+        
+    def updateParameters(self):
+        """Wrap jitcode's set_paramters to set symbolic parameter values post-compilation"""
+        if self.frozen: return
+        self.ODE.set_parameters(*[rate.value for rate in self.rates])
+    
+    def compileODE(self, integrator='dopri5',**kwargs):
+        """
+        Compile equations with selected integrator
+        
+        Options for integrator include at least:
+            'dopri5','RK45','dop853','RK23','BDF','lsoda','LSODA','Radau','vode'
+        """
+        if self.frozen:
+            self.ODE = jc.jitcode(self.eqns,**kwargs)
+        else:
+            self.ODE = jc.jitcode(self.eqns, control_pars=[rate.sym for rate in self.rates],**kwargs)
+        
+        self.ODE.set_integrator(integrator)
+    
+    def solveODE(self):
+        """Initialize and run compiled solver"""
+        inits = {v.y:v.init for v in self.strands+self.primers}
+        self.ODE.set_initial_value(inits,0.0)
+        cycles = np.arange(0.0,self.cycles,1)
+        values = {str(v):np.zeros(len(cycles)) for v in self.strands+self.primers}
+        for i,c in enumerate(cycles):
+            self.ODE.integrate(c)
+            for v in self.strands+self.primers:
+                values[str(v)][i] = self.ODE.y_dict[v.y]
+        self.solution = values
+        return values
+    
+    def timeIntegrators(self,integrators=['dopri5','RK45','dop853','RK23','BDF','lsoda','LSODA','Radau','vode'],
+                        compilation_time=False, with_update=False):
+        '''
+        Times the execution (and optionally compilation) of various ODE integrators
+        Results for a three-competitor, four-primer system with no parameter updating:
+        dopri5:
+        compile: 951 ms ± 48.4 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 34.3 ms ± 5.48 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        RK45:
+        compile: 945 ms ± 13.4 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 40 ms ± 1.56 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        dop853:
+        compile: 981 ms ± 67.7 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 31.9 ms ± 950 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        RK23:
+        compile: 956 ms ± 14.5 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 41.9 ms ± 1.14 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        BDF:
+        compile: 3.12 s ± 114 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 80.9 ms ± 2.4 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        lsoda:
+        compile: 3.41 s ± 373 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 30.7 ms ± 1.14 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        LSODA:
+        compile: 3.39 s ± 218 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 44.7 ms ± 7.32 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        Radau:
+        compile: 3.51 s ± 219 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 75.2 ms ± 5.95 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        vode:
+        compile: 3.23 s ± 147 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+        solve: 48.3 ms ± 21.7 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+        for integrator in integrators:
+            print(integrator+':')
+            if compilation_time:
+                print('compile: ',end='')
+                %timeit self.compileODE(integrator=integrator,verbose=False)
+            else:
+                self.compileODE(integrator=integrator,verbose=False)
+            if with_update:
+                self.updateParameters()
+            print('solve: ',end='')
+            %timeit self.solveODE()
+            print()
+        '''
+        pass
     
     ################################################################################
     ## Solution plotting functions
@@ -245,11 +386,11 @@ class CompetitiveReaction:
                 ind_axs = []
             else:
                 fig,ax = plt.subplots(1,1)
-
+        sweep_solutions = {i:{} for i,_ in iterator}
         for i,INT_0 in iterator:
             self.set_oligo_init(INT,10**INT_0)
-            initial = self.initialize()
-            self.solve(initial)
+            self.updateParameters()
+            self.solveODE()
             if indiv:
                 with plt.rc_context({'axes.labelweight':'normal','font.size':14}):
                     ind_axs.append(fig.add_subplot(gs[i//3,i%3], sharey=ind_axs[0] if i>0 else None))
@@ -351,7 +492,7 @@ class CompetitiveReaction:
             self.set_oligo_init(EXT,10**kwargs[str(EXT)])
         for p in self.primers:
             self.set_primer_init(p,kwargs[str(p)])
-        self.norm = self.molar2copies(kwargs['norm']*10**-9)
+        self.norm = molar2copies(kwargs['norm']*10**-9)
         self.cycles = kwargs['cycles']
         self.INT_rng = self.INT_rng_widget.value
         self.INT_res = kwargs['INT_res']
@@ -371,9 +512,9 @@ class CompetitiveReaction:
                                                description=f'{str(rate)[2:]} rate', continuous_update=False)
                      for rate in self.rates
                  },**{
-                     str(p_0)[:-2]:ipw.FloatSlider(min=1, max=500, step=25, value=p_0.value,
-                                                description=f'nM {str(p_0)[:-2]}', continuous_update=False)
-                     for p_0 in self.primer_inits
+                     str(p):ipw.FloatSlider(min=1, max=500, step=25, value=self.get_primer_init(p),
+                                                description=f'nM {str(p)}', continuous_update=False)
+                     for p in self.primers
                  },**{
                      str(EXT):ipw.FloatSlider(min=0, max=10, step=0.25, value=np.log10(self.get_oligo_init(EXT)),
                                                 description=f'logs {str(EXT)}', continuous_update=False)
